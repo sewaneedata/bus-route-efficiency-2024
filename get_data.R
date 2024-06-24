263968ed179f0fa377e9693e37acbcfba46da9a5
Mason Dickens <masondickens@Masons-MacBook-Air-2.local>
2024-06-24 10:07:45 -0500

    new commit

--- get_data.R
+++ get_data.R
@@ -6,3 +6,18 @@ library(gsheet)
 #load data
 
 bus_df<- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1jXgmUvc1uVJFxSnDV9dYZgEJZjoZ7Bqq/edit?usp=sharing&ouid=103805330628828323032&rtpof=true&sd=true")
+
+# Number of columns
+ncol(bus_df)
+
+# Number of rows
+nrow(bus_df)
+
+# Name of variables
+names(bus_df)
+
+#First rows 
+head(bus_df)
+
+#Last rows
+tail(bus_df)
