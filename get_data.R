d1efefd52b7f71866a3f17527c199051edb1d663
Mason Dickens <masondickens@Masons-MacBook-Air-2.local>
2024-06-24 10:03:53 -0500

    New commit

--- get_data.R
+++ get_data.R
@@ -5,4 +5,4 @@ library(gsheet)
 
 #load data
 
-bus_df<- gsheet::ghseet2tbl("https://docs.google.com/spreadsheets/d/1jXgmUvc1uVJFxSnDV9dYZgEJZjoZ7Bqq/edit?usp=sharing&ouid=103805330628828323032&rtpof=true&sd=true")
+bus_df<- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1jXgmUvc1uVJFxSnDV9dYZgEJZjoZ7Bqq/edit?usp=sharing&ouid=103805330628828323032&rtpof=true&sd=true")
