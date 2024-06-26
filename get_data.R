2218b681abd02f272c9f25c6fda9eb191e09bc9c
Shiraz Hattan Robinson II <shirazrobinsonii@Shirazs-MacBook-Air.local>
2024-06-26 11:24:38 -0500

    updated git ignore and the data retreival method

--- get_data.R
+++ get_data.R
@@ -1,11 +1,15 @@
 # load libraries
 
 library(tidyverse)
-library(gsheet)
+library(googledrive)
+library(readxl)
+
 
 #load data
 
-bus_df<- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1jXgmUvc1uVJFxSnDV9dYZgEJZjoZ7Bqq/edit?usp=sharing&ouid=103805330628828323032&rtpof=true&sd=true")
+drive_download("https://docs.google.com/spreadsheets/d/1jXgmUvc1uVJFxSnDV9dYZgEJZjoZ7Bqq/edit?usp=sharing&ouid=112944113011797673714&rtpof=true&sd=true", path = "data/roster_tally.xls", overwrite = TRUE)
+
+bus_df <- read_excel("data/roster_tally.xls")
 
 # Number of columns
 ncol(bus_df)
@@ -24,16 +28,13 @@ tail(bus_df)
 
 # Data key 
 # BV <- Broadview Elementary School
-# CM <-Clark Memorial Elementary School
-# C  <-Cowan Elementary School
-# D  <-Decherd Elementary School
-# FC <-Franklin County High School
-# H  <-Huntland Schools
-# NL <-North Lake Elementary
-# NM <-North Middle School
-# RC <-Rock Creek Elementary School
-# S  <-Sewanee Elementary School
-# SM <-South Middle School
-
-
-
+# CM <- Clark Memorial Elementary School
+# C  <- Cowan Elementary School
+# D  <- Decherd Elementary School
+# FC <- Franklin County High School
+# H  <- Huntland Schools
+# NL <- North Lake Elementary
+# NM <- North Middle School
+# RC <- Rock Creek Elementary School
+# S  <- Sewanee Elementary School
+# SM <- South Middle School
