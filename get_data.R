6507f5aae0c571e73711ab1469d090d34bc04dba
Shiraz Hattan Robinson II <shirazrobinsonii@Shirazs-MacBook-Air.local>
2024-07-01 11:07:47 -0500

    moving data into data folder and pushing

--- get_data.R
+++ get_data.R
@@ -9,7 +9,7 @@ library(geodata)
 bus_routes <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/12Qj9yy1YgqnOWQk3qDCRP9LjsEQUckdJz9DPwogGDSM/edit?pli=1&gid=0#gid=0") %>% 
   mutate( Address = paste0( Address, " 'Franklin County' TN" ) )
 
-load("bus_gis.RData")
+load("data/bus_gis.RData")
 
 bus_routes <- left_join( bus_points %>% select( Address = address, address_google, location_type, pnt ), bus_routes, by = "Address")
 bus_routes$Bus <- factor( as.character(bus_routes$Bus) )
