89c8799c18dcf9d9d1516d7f84ffcd709762ca1f
Shiraz Hattan Robinson II <shirazrobinsonii@Shirazs-MacBook-Air.local>
2024-07-02 10:52:32 -0500

    Code with comments.

--- get_data.R
+++ get_data.R
@@ -1,3 +1,5 @@
+# load libraries
+
 library(sf)
 library(tmap)
 library(osmdata)
@@ -6,6 +8,8 @@ library(tidyverse)
 library(mapsapi)
 library(geodata)
 
+#load bus data
+
 bus_routes <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/12Qj9yy1YgqnOWQk3qDCRP9LjsEQUckdJz9DPwogGDSM/edit?pli=1&gid=0#gid=0") %>% 
   mutate( Address = paste0( Address, " 'Franklin County' TN" ) )
 
@@ -13,6 +17,8 @@ load("data/bus_gis.RData")
 
 bus_routes <- left_join( bus_points %>% select( Address = address, address_google, location_type, pnt ), bus_routes, by = "Address")
 bus_routes$Bus <- factor( as.character(bus_routes$Bus) )
+
+# make bus map
 
 tmap_mode( "view" )
 tmap_options( basemaps = providers$OpenStreetMap )
