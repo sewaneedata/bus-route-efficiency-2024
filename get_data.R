6aace08f7a006c2eb257b35a2dccbf7f25e6dabf
Mason Dickens <masondickens@Masons-MacBook-Air-2.local>
2024-07-02 11:25:16 -0500

    adding comments

--- get_data.R
+++ get_data.R
@@ -1,5 +1,4 @@
 # load libraries
-
 library(sf)
 library(tmap)
 library(osmdata)
@@ -9,13 +8,15 @@ library(mapsapi)
 library(geodata)
 
 #load bus data
-
 bus_routes <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/12Qj9yy1YgqnOWQk3qDCRP9LjsEQUckdJz9DPwogGDSM/edit?pli=1&gid=0#gid=0") %>% 
   mutate( Address = paste0( Address, " 'Franklin County' TN" ) )
 
+#Need to ask Dr.Rudd about this data.
 load("data/bus_gis.RData")
 
+#Need to ask Dr.Rudd what these lines do.
 bus_routes <- left_join( bus_points %>% select( Address = address, address_google, location_type, pnt ), bus_routes, by = "Address")
+#Convert bus into levels.
 bus_routes$Bus <- factor( as.character(bus_routes$Bus) )
 
 # make bus map
@@ -27,3 +28,4 @@ tm_shape( franklin ) +
   tm_polygons( alpha = 0.5, lwd=3 ) +
   tm_shape( bus_routes ) +
   tm_dots( col="Bus" )
+
