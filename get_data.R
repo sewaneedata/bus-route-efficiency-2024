d42d0dbf873d4a033b9591f412a150446c1d4a0f
Srijan Basnet <srijanbasnet@Srijans-MacBook-Pro.local>
2024-07-09 15:51:29 -0500

    idk

--- get_data.R
+++ get_data.R
@@ -49,6 +49,7 @@ bus_routes_summary <- bus_routes %>%
   group_by(Bus) %>%
   summarize(num_kid_per_bus = n()) %>%
   ungroup()
+bus_routes_summary<- st_drop_geometry(bus_routes_summary)
 
 # Convert Bus numbers to factors to enable automatic coloring on the map
 bus_routes$Bus <- factor(bus_routes$Bus, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
